718cf051adb5e329aedaad6eed494a2af3dc51d1
Schzium <35246037+Schzium@users.noreply.github.com>
2026-08-15 00:35:22 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'sal-app-v62';
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).then(r => {
+    if (r && r.status === 200 && e.request.method === 'GET') {
+      const clone = r.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+    }
+    return r;
+  }).catch(() => caches.match(e.request)));
+});
